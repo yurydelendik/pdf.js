abf7036bed463baa3cbcc927d472a623c0481c05
Tim van der Meij <timvandermeij@gmail.com>
2014-04-07 22:27:00 +0200

    Fixing non-equivalent statements in src/shared/annotation.js

--- src/shared/annotation.js
+++ src/shared/annotation.js
@@ -112,7 +112,8 @@ var Annotation = (function AnnotationClosure() {
           var isInvalid = false;
           var numPositive = 0;
           for (var i = 0; i < dashArrayLength; i++) {
-            if (+dashArray[i] < 0) {
+            var valid = (+dashArray[i] >= 0);
+            if (!valid) {
               isInvalid = true;
               break;
             } else if (dashArray[i] > 0) {
